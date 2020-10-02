51e761f9d6db2296a93fd8a5aa1bbd2675da100e
Rebeca Calazans <rebecac.brito@gmail.com>
2020-10-02 16:25:28 -0300

    Update siteConfig.js
    Update data

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Rebeca Calazans de Brito', // Title for your website.
+    tagline: 'I\'m doing this GHC workshop.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://rebecacalazans.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'rebecacalazans',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'rebeca-calazans-1a06b1bb',
         twitter: '',
-        github: '',
+        github: 'rebecacalazans',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
